27ce2ffcad89e16093856b037c9e99b2428c3379
Alexandre Emond <143522099+a3emond@users.noreply.github.com>
2025-03-19 14:34:49 -0400

    add connection methods and properties to IDatabaseConnection and DatabaseConnection

--- src/main/java/com/fortisbank/data/database/DatabaseConnection.java
+++ src/main/java/com/fortisbank/data/database/DatabaseConnection.java
@@ -1,4 +1,29 @@
 package com.fortisbank.data.database;
+// TODO: Singleton pattern
+public class DatabaseConnection implements IDatabaseConnection {
+    private final String _connectionString = "";
+    private final Object _connection = null;
 
-public class DatabaseConnection {
+    public DatabaseConnection(String connectionString) {}
+
+    public DatabaseConnection() {}
+
+    @Override
+    public void Connect() { //TODO: Implement this method
+        //if (!TestConnection()) return null; // if connection is not valid return null
+        // create connection object
+        // return connection object
+        //return _connection;
+    }
+
+    @Override
+    public void Disconnect() { //TODO: Implement this method
+
+    }
+
+    // test connection
+    public boolean TestConnection() { //TODO: Implement this method
+        // if is valid connection
+        return true;
+    }
 }
--- src/main/java/com/fortisbank/data/database/IDatabaseConnection.java
+++ src/main/java/com/fortisbank/data/database/IDatabaseConnection.java
@@ -1,4 +1,10 @@
 package com.fortisbank.data.database;
 
 public interface IDatabaseConnection {
+
+    String _connectionString = "";
+    Object _connection = null;
+
+    public void Connect();
+    public void Disconnect();
 }
