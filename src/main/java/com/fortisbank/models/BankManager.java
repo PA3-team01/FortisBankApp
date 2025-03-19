6cd7be6a018f91988e26417e8930b9efe491c840
FlavyOria <100291927+FlavyOria@users.noreply.github.com>
2025-03-19 15:24:19 -0400

    add V1 of Transaction+Account with correct name, constructor, get+set based on the diagram.

--- src/main/java/com/fortisbank/models/BankManager.java
+++ src/main/java/com/fortisbank/models/BankManager.java
@@ -1,4 +1,27 @@
 package com.fortisbank.models;
 
 public class BankManager {
+    private String name;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void displayInfo() {
+        System.out.println("Bank Manager Name: " + name);
+        System.out.println("Bank Manager ID: " + id);
+    }
 }
