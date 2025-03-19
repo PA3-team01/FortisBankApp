af2f9a87e51a2bddbe7e5edb3532cc98dd460eae
jadekelly1 <kjade0739@gmail.com>
2025-03-19 14:11:42 -0400

    get/set Customer

--- src/main/java/com/fortisbank/models/Customer.java
+++ src/main/java/com/fortisbank/models/Customer.java
@@ -1,4 +1,58 @@
 package com.fortisbank.models;
 
 public class Customer {
+    private String CustomerID;
+    private String FirstName;
+    private String LastName;
+    private String PINHash;
+    private String Email;
+    private String PhoneNumber;
+
+    public String getCustomerID() {
+        return CustomerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.CustomerID = customerID;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.LastName = lastName;
+    }
+
+    public String getPINHash() {
+        return PINHash;
+    }
+
+    public void setPINHash(String PINHash) {
+        this.PINHash = PINHash;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        this.Email = email;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.PhoneNumber = phoneNumber;
+    }
 }
