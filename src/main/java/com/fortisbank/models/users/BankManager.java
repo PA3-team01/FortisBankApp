d570f0573d1c69b86b723e125944073a29de2200
Alexandre Emond <143522099+a3emond@users.noreply.github.com>
2025-03-29 17:17:38 -0400

    added getters and setters in BankManager

--- src/main/java/com/fortisbank/models/users/BankManager.java
+++ src/main/java/com/fortisbank/models/users/BankManager.java
@@ -27,6 +27,56 @@ public class BankManager extends User implements Serializable {
         this.customers = new ArrayList<>();
     }
 
+    // ------------------------- SETTERS -------------------------
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+    public void setPINHash(String pinHash) {
+        this.PINHash = pinHash;
+    }
+    public void setCustomers(List<Customer> customers) {
+        this.customers.clear();
+        this.customers.addAll(customers);
+    }
+    public void addCustomer(Customer customer) {
+        this.customers.add(customer);
+    }
+    public void removeCustomer(Customer customer) {
+        this.customers.remove(customer);
+    }
+    // ------------------------- GETTERS -------------------------
+    public String getUserId() {
+        return userId;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+    public String getPINHash() {
+        return PINHash;
+    }
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    // ------------------------- toString -------------------------
     @Override
     public String toString() {
         return "BankManager{" +
