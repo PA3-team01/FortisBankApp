07f18d66960891eea9a6a7197361f1883b204f71
Alexandre Emond <143522099+a3emond@users.noreply.github.com>
2025-04-02 09:07:09 -0400

    feat(ui): extend ProfilePanel to support user profile updates with session management

--- src/main/java/com/fortisbank/ui/panels/commons/ProfilePanel.java
+++ src/main/java/com/fortisbank/ui/panels/commons/ProfilePanel.java
@@ -1,4 +1,11 @@
 package com.fortisbank.ui.panels.commons;
 
-public class ProfilePanel {
+import javax.swing.*;
+
+public class ProfilePanel extends JPanel {
+    // will be used to update the current user profile
+
+    //require session manager to get current user and storage mode need to be passed to the constructor
+    //uses Customer service and bank manager service depending on role to update
+
 }
